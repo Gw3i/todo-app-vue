37f9fa71ad38f50a54637989e519e5375da188d4
Vlady <contact.johngoldco@gmail.com>
2022-09-16 12:57:06 +0200

    Add form

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <h1>Hello, World!</h1>
+  <main>
+    <h1>Todo App</h1>
+    <p>Create a list of tasks</p>
+
+    <form>
+      <label>
+        <input
+          type="text"
+          placeholder="Add new task"
+          v-model="newTask"
+          @keypress.enter="addTask"
+        />
+      </label>
+      <button @click="addTask">Add</button>
+    </form>
+  </main>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage',
-}
+export default {}
 </script>
